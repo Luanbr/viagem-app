058708edef05a20544079d42d9702bf132518cef
luanbramalho <luan.b.ramalho@gmail.com>
2023-07-20 14:58:13 -0300

    Alter import.sql

--- agencia-app/src/main/resources/import.sql
+++ agencia-app/src/main/resources/import.sql
@@ -1,4 +1,5 @@
---CREATE SEQUENCE hibernate_sequence start with 1 increment by 1;
+CREATE SEQUENCE book_seq start with 1 increment by 1;
+
 INSERT INTO Client (id, name) VALUES (1, 'João');
 INSERT INTO Client (id, name) VALUES (2, 'Maria');
 INSERT INTO Client (id, name) VALUES (3, 'José');
